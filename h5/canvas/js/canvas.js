779f290746d184295ca902cc4b91fcd288a97c05
lijc <wb-lijc@alipay.com>
2013-10-31 16:16:46 +0800

    canvas 直线函数

--- h5/canvas/js/canvas.js
+++ h5/canvas/js/canvas.js
@@ -1,6 +1,16 @@
-$(function(){
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-})
+//画直线 原点始终在(0,0),使用变换来移动
+function drawLine(ctx,options){
+	ctx.beginPath();
+	ctx.moveTo(0,0);
+	for(var spot in options){
+		ctx.lineTo(options[spot].x,options[spot].y);
+	}
+	ctx.stroke();
+}
+//画树冠
+function drawTree(ctx){
+
+}
+
 
 
--- h5/canvas/js/translate.js
+++ h5/canvas/js/translate.js
@@ -1,21 +1,14 @@
-
 $(function(){
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 
 	//普通的简单绘图
-	ctx.beginPath();
-	ctx.moveTo(0,0);
-	ctx.lineTo(50,50);
-	ctx.stroke();
+	drawLine(ctx,[{x:50,y:50}]);
 	//通过变换来绘图，好处是更方便的坐标计算
 	//在canvas中使用变换之前需要保存当前的canvas对象，在完成绘图以后要恢复原有的状态
 	ctx.save();
-	ctx.translate(50,50);
-	ctx.beginPath();
-	ctx.moveTo(0,0);
-	ctx.lineTo(50,50);
-	ctx.stroke();
+	ctx.translate(60,60);
+	drawLine(ctx,[{x:60,y:60},{x:90,y:45},{x:110,y:99}]);
 	ctx.restore();
 
 })
