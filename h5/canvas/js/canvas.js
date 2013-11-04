1d6239e50951c28d1c9320ccbf85e771337e863d
lijc <wb-lijc@alipay.com>
2013-11-04 16:16:14 +0800

    litian

--- h5/canvas/js/canvas.js
+++ h5/canvas/js/canvas.js
@@ -1,13 +1,13 @@
-/*画直线
-* origin {array}  线的起始点
-*  options {arrayList} 点数组的集合
-*  closed {boolean} 是否闭合
+/*画路径
+*  param {array}  origin 线的起始点
+*  param {arrayList} options 路径经过点的集合
+*  param {boolean} closed 是否闭合
 */
-function drawLine(ctx,origin,options,closed){
+function drawLine(ctx,origin,throughPoints,closed){
 	ctx.beginPath();
 	ctx.moveTo(origin[0],origin[1]);
-	for(var spot in options){
-		ctx.lineTo(options[spot][0],options[spot][1]);
+	for(var spot=0;spot<throughPoints.length;spot++){
+		ctx.lineTo(throughPoints[spot][0],throughPoints[spot][1]);
 	}
 	if(closed){
 		ctx.closePath();
@@ -17,21 +17,32 @@ function drawLine(ctx,origin,options,closed){
 function drawTree(ctx){
 	ctx.save();
 	ctx.translate(130,250);
+	//树冠路径
 	drawLine(ctx,[-25,-50],[[-10,-80],[-20,-80],[-5,-110],[-15,-110],[0,-140],[15,-110],[5,-110],[20,-80],[10,-80],[25,-50]],true);
 	ctx.lineWidth = 4;
 	ctx.lineJoin = "round";
 	ctx.strokeStyle = "#663300";
+	ctx.stroke();
 	ctx.fillStyle = "#339900";
 	ctx.fill();
 	ctx.fillStyle = "#663300";
 	ctx.fillRect(-5,-50,10,50);
-	ctx.restore();
-	ctx.stroke();
+	ctx.restore()
 }
 /* 画曲线
 * */
-function draw(ctx){
+function drawQuadraticCurve(ctx){
+	ctx.save();
+	ctx.translate(-10,420);
+	ctx.beginPath();
+	ctx.moveTo(0,0);
+	ctx.quadraticCurveTo(170,-50,300,-210);
+	ctx.quadraticCurveTo(460,-400,530,-400);
 
+	ctx.strokeStyle = "#663300";
+	ctx.lineWidth = 20;
+	ctx.stroke();
+	ctx.restore();
 }
 
 
--- h5/canvas/js/translate.js
+++ h5/canvas/js/translate.js
@@ -2,9 +2,6 @@ $(function(){
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 
-	//普通的简单绘图
-	drawLine(ctx,[0,0],[[50,50]]);
-	ctx.stroke();
 	//通过变换来绘图，好处是更方便的坐标计算
 	//在canvas中使用变换之前需要保存当前的canvas对象，在完成绘图以后要恢复原有的状态
 	/*ctx.save();
@@ -14,5 +11,6 @@ $(function(){
 
 	//树
 	drawTree(ctx);
-
+	//曲线
+	drawQuadraticCurve(ctx);
 })
