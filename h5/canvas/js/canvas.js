4f3ca41d3aa1c55ce9f382c5726a2531e2810291
lijc <wb-lijc@alipay.com>
2013-11-04 14:56:19 +0800

    litian

--- h5/canvas/js/canvas.js
+++ h5/canvas/js/canvas.js
@@ -1,14 +1,36 @@
-//画直线 原点始终在(0,0),使用变换来移动
-function drawLine(ctx,options){
+/*画直线
+* origin {array}  线的起始点
+*  options {arrayList} 点数组的集合
+*  closed {boolean} 是否闭合
+*/
+function drawLine(ctx,origin,options,closed){
 	ctx.beginPath();
-	ctx.moveTo(0,0);
+	ctx.moveTo(origin[0],origin[1]);
 	for(var spot in options){
-		ctx.lineTo(options[spot].x,options[spot].y);
+		ctx.lineTo(options[spot][0],options[spot][1]);
 	}
+	if(closed){
+		ctx.closePath();
+	}
+}
+//画树
+function drawTree(ctx){
+	ctx.save();
+	ctx.translate(130,250);
+	drawLine(ctx,[-25,-50],[[-10,-80],[-20,-80],[-5,-110],[-15,-110],[0,-140],[15,-110],[5,-110],[20,-80],[10,-80],[25,-50]],true);
+	ctx.lineWidth = 4;
+	ctx.lineJoin = "round";
+	ctx.strokeStyle = "#663300";
+	ctx.fillStyle = "#339900";
+	ctx.fill();
+	ctx.fillStyle = "#663300";
+	ctx.fillRect(-5,-50,10,50);
+	ctx.restore();
 	ctx.stroke();
 }
-//画树冠
-function drawTree(ctx){
+/* 画曲线
+* */
+function draw(ctx){
 
 }
 
--- h5/canvas/js/translate.js
+++ h5/canvas/js/translate.js
@@ -3,12 +3,16 @@ $(function(){
 	var ctx = canvas.getContext("2d");
 
 	//普通的简单绘图
-	drawLine(ctx,[{x:50,y:50}]);
+	drawLine(ctx,[0,0],[[50,50]]);
+	ctx.stroke();
 	//通过变换来绘图，好处是更方便的坐标计算
 	//在canvas中使用变换之前需要保存当前的canvas对象，在完成绘图以后要恢复原有的状态
-	ctx.save();
+	/*ctx.save();
 	ctx.translate(60,60);
-	drawLine(ctx,[{x:60,y:60},{x:90,y:45},{x:110,y:99}]);
-	ctx.restore();
+	drawLine(ctx,[0,0],[[60,60],[90,45],[110,99]]);
+	ctx.restore();*/
+
+	//树
+	drawTree(ctx);
 
 })
